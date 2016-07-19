620a763628850541c995ff02b9af74596c26e79c
zoe <zhu_hp@yeah.net>
2016-07-19 16:02:04 +0800

    first commit

---
+++ src/com/test/action/Hello.java
@@ -0,0 +1,23 @@
+package com.test.action;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.AbstractController;
+
+public class Hello extends AbstractController {
+
+	@Override
+	protected ModelAndView handleRequestInternal(HttpServletRequest request, HttpServletResponse response)
+			throws Exception {
+
+		String helloStr = request.getParameter("hello");
+
+		System.out.print("-----helloStr-----" + helloStr);
+
+		ModelAndView helloView = new ModelAndView("hello");
+		helloView.addObject("HelloWorld", helloStr);
+		return helloView;
+	}
+}
