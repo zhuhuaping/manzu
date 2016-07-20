ef5e8febfab5d7e6ec511b0efdbb56a2b96b88ad
zoe <zhu_hp@yeah.net>
2016-07-20 17:59:52 +0800

    spring jdbc 中途版

---
+++ src/com/model/User.java
@@ -0,0 +1,20 @@
+/**
+ *
+ */
+package com.model;
+
+import java.io.Serializable;
+
+/**
+ * @author ZhuHuaPing
+ *
+ */
+public class User implements Serializable {
+
+	/**
+	 * serialVersionUID¡£<br>
+	 *
+	 */
+	private static final long serialVersionUID = -2020674679L;
+
+}
--- test/com/service/impl/Hello.java
+++ test/com/service/impl/Hello.java
@@ -1,4 +1,4 @@
-package com.test.action;
+package com.service.impl;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
